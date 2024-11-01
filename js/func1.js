98dde07399fda77fd2d1003d91fd6fbb1a1fcff5
scrimblini <102263436+scrimblini@users.noreply.github.com>
2024-11-01 01:20:15 -0300

    Fixed the menu design, modified content toggle so it alternates between a plus and minus sign

--- js/func1.js
+++ js/func1.js
@@ -4,7 +4,9 @@ let contentsMenu = document.getElementById("mainarea");
 menuUnfold.addEventListener("click", function () {
     if (contentsMenu.style.display === "none" || contentsMenu.style.display === "") {
         contentsMenu.style.display = "block";
+        menuUnfold.innerText = "-";
     } else {
         contentsMenu.style.display = "none";
+        menuUnfold.innerText = "+";
     }
 });
