338fd33a8a9ec5cbf40a8bdceaa0901d0608a9f2
scrimblini <102263436+scrimblini@users.noreply.github.com>
2024-11-01 03:31:44 -0300

    Made design more smooth and began working on bio

--- js/func1.js
+++ js/func1.js
@@ -5,8 +5,10 @@ menuUnfold.addEventListener("click", function () {
     if (contentsMenu.style.display === "none" || contentsMenu.style.display === "") {
         contentsMenu.style.display = "block";
         menuUnfold.innerText = "-";
+        document.getElementById("titlemain").style.marginTop = "0"
     } else {
         contentsMenu.style.display = "none";
         menuUnfold.innerText = "+";
+        document.getElementById("titlemain").style.marginTop = "15%"
     }
 });
