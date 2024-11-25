363e33d3500322a0e19652197aacde268ce3f392
scrimblini <102263436+scrimblini@users.noreply.github.com>
2024-11-25 10:57:56 -0300

    Added Tour and Film pages, cleaned up javascript form validation and other details

--- js/func1.js
+++ js/func1.js
@@ -119,3 +119,38 @@ function hideDesc() {
               openItem(title, content, imgSrc);
             });
           });
+
+
+//--------------------------form--------------------------
+
+document.getElementById('newslform').addEventListener('submit', function(event) {
+    event.preventDefault();
+    submitData();
+  });
+  
+  function submitData() {
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    
+    if (!name || !email || !emailPattern.test(email)) {
+      document.getElementById("popupWindow").style.display = "flex";
+      document.getElementById("windowTitl").innerText = 'Error';
+      document.getElementById("windowP").innerText = 'Please enter the corresponding data in the required fields.';
+    } else {
+      console.log("Form submitted");
+      document.getElementById("popupWindow").style.display = "flex";
+      document.getElementById("windowTitl").innerText = 'Success!';
+      document.getElementById("windowP").innerText = 'The data was submitted successfully.';
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
+    }
+  }
+  
+  function closeWindow() {
+    document.getElementById("windowTitl").innerText = '';
+    document.getElementById("windowP").innerText = '';
+    document.getElementById("popupWindow").style.display = "none";
+  }
+  
